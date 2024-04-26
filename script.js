fa456177059689901f370717cd98c8aa9c8247bc
TomVerez531000 <tommverez@gmail.com>
2024-04-26 17:28:30 +0200

    updated and checkbox is now working!

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 registerbtn = document.getElementsByClassName("reg")[0]
 loginbtn = document.getElementsByClassName("back")[0]
 
+secondsleft = document.getElementsByClassName("secs")[0]
+checkbox = document.getElementsByClassName("agreecheckbox")[0]
+timereaming = 5
+aldreadyclicked = false
+
+function timerdiscount(){
+    timereaming -= 1
+    secondsleft.innerHTML = String(timereaming)+" seconds reaming."
+    if (timereaming > 0) {
+        setTimeout(() => timerdiscount(), 1000);
+    }else{
+        secondsleft.style.display = "none";
+        checkbox.disabled = false
+    }
+}
+
 function noaccountclicked() {
     div = document.getElementsByClassName("logincontainer")[0]
     div.classList.add("slide-out");
@@ -8,6 +24,11 @@ function noaccountclicked() {
     div2 = document.getElementsByClassName("registercontainer")[0]
     div2.classList.add("slide-out");
     div2.classList.remove("slide-in");
+
+    if (!aldreadyclicked) {
+        aldreadyclicked = true
+        setTimeout(() => timerdiscount(), 1000);
+    }
 }
 
 function backclicked() {
